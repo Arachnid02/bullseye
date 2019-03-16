d02289a65cd189fec3a94218b32222463cf3629c
Arachnid02 <jianwen.chen@uamaker.nyc>
2019-03-15 20:41:18 -0400

    Size, position, and color of circle will be totally random

--- bullseye.py
+++ bullseye.py
@@ -1,11 +1,16 @@
 from graphics import *
+from random import *
 
 def mk_circle(rad, cirX, cirY, color, window):
     bullRing = Circle(Point(cirX, cirY), rad)
     bullRing.setFill(color)
     bullRing.draw(window)
 
+rad = randint(10, 100)
+cirXY = randint(200, 400)
+color = color_rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+
 window = GraphWin("Frankie Boi's Bullseye", 600, 600)
 window.setCoords(0, 0, 600, 600)
 
-mk_circle(50, 300, 300, "red", window)
+mk_circle(rad, cirXY, cirXY, color, window)
