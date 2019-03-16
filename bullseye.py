0f60df30e9259805df43e0a2454b61dc370abd32
Arachnid02 <jianwen.chen@uamaker.nyc>
2019-03-15 21:25:53 -0400

    Multiple bullseyes are created through the use of a for loop

--- bullseye.py
+++ bullseye.py
@@ -1,16 +1,26 @@
 from graphics import *
 from random import *
 
-def mk_circle(rad, cirX, cirY, color, window):
-    bullRing = Circle(Point(cirX, cirY), rad)
+def mk_circle(rad, cirXY, color, window):
+    bullRing = Circle(Point(cirXY, cirXY), rad)
     bullRing.setFill(color)
     bullRing.draw(window)
 
-rad = randint(10, 100)
+rad = randint(10, 20)
 cirXY = randint(200, 400)
-color = color_rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+counter = 5
 
 window = GraphWin("Frankie Boi's Bullseye", 600, 600)
 window.setCoords(0, 0, 600, 600)
 
-mk_circle(rad, cirXY, cirXY, color, window)
+for i in range(counter):
+    mk_circle(rad * counter,
+              cirXY,
+              color_rgb(randint(0, 255),
+                        randint(0, 255),
+                        randint(0, 255)),
+              window)
+    counter -= 1
+
+window.getMouse()
+window.close()
